6c3e8e95aec36f1e01d490714262608f491688ee
Denis Romanenko <romadenn@gmail.com>
2019-10-06 16:25:21 +0300

    first commit

---
+++ routes/cards.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+
+const sendCards = (req, res) => {
+  const dataPath = path.join(__dirname, `../data/cards.json`);
+  fs.readFile(dataPath, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send(JSON.parse(data));
+  });
+};
+router.get("/cards", sendCards);
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-useless-return */
+const router = require("express").Router();
+const path = require("path");
+const fs = require("fs");
+
+const sendUsers = (req, res) => {
+  const dataPath = path.join(__dirname, `../data/users.json`);
+  fs.readFile(dataPath, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.send(JSON.parse(data));
+  });
+};
+
+const sendUsersId = (req, res) => {
+  const dataPath = path.join(__dirname, `../data/users.json`);
+  fs.readFile(dataPath, { encoding: "utf8" }, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const users = JSON.parse(data);
+
+    for (let i = 0; i < users.length; i += 1) {
+      // eslint-disable-next-line no-underscore-dangle
+      if (users[i]._id === req.params.id) {
+        res.send(users[i]);
+      }
+    }
+    res.status(404).send({ message: "Нет пользователя с таким id" });
+    return;
+  });
+};
+router.get("/:someRequest", (req, res) => {
+  if (req.params.someRequest) {
+    res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
+    return;
+  }
+});
+
+router.get("/users", sendUsers);
+router.get("/users/:id", sendUsersId);
+
+module.exports = router;
