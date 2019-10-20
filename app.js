f5e576bdb0859f949c17bcb8cef3b0b8a2ba6f19
Denis Romanenko <romadenn@gmail.com>
2019-10-20 10:31:46 +0300

    add mongo

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 /* eslint-disable quotes */
 const express = require("express");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/mestodb", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 const cards = require("./routes/cards");
 const users = require("./routes/users");
@@ -9,6 +17,14 @@ const { PORT = 3000 } = process.env;
 const app = express();
 
 app.use(express.static("public"));
+app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: "5da786d1b37f99218899ba69"
+  };
+
+  next();
+});
 
 app.use("/", cards);
 app.use("/", users);
--- routes/users.js
+++ routes/users.js
@@ -1,43 +1,21 @@
 /* eslint-disable quotes */
 /* eslint-disable no-useless-return */
 const router = require("express").Router();
-const path = require("path");
-const fs = require("fs");
 
-const sendUsers = (req, res) => {
-  const dataPath = path.join(__dirname, `../data/users.json`);
-  fs.readFile(dataPath, { encoding: "utf8" }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    res.send(JSON.parse(data));
-  });
-};
+const {
+  getUsers,
+  getUsersId,
+  postUser,
+  patchProfile,
+  patchAvatar
+} = require("../controllers/users");
 
-const sendUsersId = (req, res) => {
-  const dataPath = path.join(__dirname, `../data/users.json`);
-  fs.readFile(dataPath, { encoding: "utf8" }, (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const users = JSON.parse(data);
+router.get("/users", getUsers);
+router.get("/users/:userId", getUsersId);
+router.post("/users", postUser);
+router.patch("/users/me", patchProfile);
+router.patch("/users/me/avatar", patchAvatar);
 
-    for (let i = 0; i < users.length; i += 1) {
-      // eslint-disable-next-line no-underscore-dangle
-      if (users[i]._id === req.params.id) {
-        res.send(users[i]);
-        return;
-      }
-    }
-    res.status(404).send({ message: "Нет пользователя с таким id" });
-    return;
-  });
-};
-
-router.get("/users", sendUsers);
-router.get("/users/:id", sendUsersId);
 router.get("/:someRequest", (req, res) => {
   res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
   return;
